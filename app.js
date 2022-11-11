d142d796e76b222045264bc95303730040d26a4b
hirsty777 <777hirsty@gmail.com>
2022-11-11 13:33:12 +0400

    finished

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const app = Vue.createApp({
-
-
+    data(){
+        return{
+            enteredTask:'',
+            tasks:[],
+            isVissible:true,
+            buttonStatus:'Hide'
+        }
+    },
+    methods:{
+        addTask(){
+            this.tasks.push(this.enteredTask);
+        },
+        showHide(){
+            this.isVissible=!this.isVissible;
+            if(this.buttonStatus=='Hide'){
+                this.buttonStatus="Show"
+            }else{
+                this.buttonStatus="Hide"
+            }
+        }
+    }
 
 });
 app.mount('#assignment');
